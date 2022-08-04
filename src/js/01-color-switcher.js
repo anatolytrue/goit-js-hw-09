abf4099f6beeee0f5d17c6a3165c1285af957098
Anatolii <anatoly.tomnyak@gmail.com>
2022-08-04 17:48:52 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+    document.body.style.background = getRandomHexColor();
+}
+
+function clearEventListener() {
+    clearInterval(timerId);
+    document.querySelector('[data-start]').disabled = false;
+    buttonStop.removeEventListener(clearEventListener)
+}
+
+buttonStart.addEventListener('click', () => {
+    timerId = setInterval(changeColor, 1000);
+    document.querySelector('[data-start]').disabled = true;
+
+    buttonStop.addEventListener('click', clearEventListener);
+})
 
