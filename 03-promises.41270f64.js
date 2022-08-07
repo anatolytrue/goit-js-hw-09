ef7a9edcd9a6562273953ee6f13907511213875d
anatolytrue <97836186+anatolytrue@users.noreply.github.com>
2022-08-07 21:55:04 +0000

    Deploying to gh-pages from @ anatolytrue/goit-js-hw-09@974d60c6f9643dac11de5e2d5e1a7818bcd82844 🚀

--- 03-promises.41270f64.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".delay"),document.querySelector(".step"),document.querySelector(".amount"),document.querySelector(".submit-btn");(void Math.random()).then((function(o){var e=o.position,t=o.delay;console.log("✅ Fulfilled promise ".concat(e," in ").concat(t,"ms"))})).catch((function(o){var e=o.position,t=o.delay;console.log("❌ Rejected promise ".concat(e," in ").concat(t,"ms"))}))}();
-//# sourceMappingURL=03-promises.41270f64.js.map
--- 03-promises.70bb192a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".delay"),document.querySelector(".step"),document.querySelector(".amount"),document.querySelector(".submit-btn");(void Math.random()).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.70bb192a.js.map
